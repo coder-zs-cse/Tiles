14d3c23c6c5d767c7e0a1849bd3bb8c66fc7a08f
coder-zs-cse <75170293+coder-zs-cse@users.noreply.github.com>
2020-12-01 10:33:16 +0530

    Create tiles_arrange.cpp

---
+++ tiles_arrange.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+void count(int n, int &t)
+{
+    if (n == 0)
+    {
+        t++;
+        return;
+    }
+    if (n - 2 >= 0)
+    {
+        count(n - 2, t);
+    }
+    if (n - 1 >= 0)
+    {
+        count(n - 1, t);
+    }
+}
+int tilingways(int n)
+{
+    int t = 0;
+    count(n, t);
+    return t;
+}
+int main()
+{
+    int N;
+    cin >> N;
+    while (N--)
+    {
+        int n;
+        cin >> n;
+        cout << tilingways(n)<<endl;
+    }
+
+    return 0;
+}
